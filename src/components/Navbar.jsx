bea8b0c0c12f5081efcf6685e6c871266ddcf89c
amankumarsingh246810 <singhamankumar924@gmail.com>
2024-08-17 22:39:34 +0530

    This is a Todo project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex justify-between bg-indigo-700 text-white py-2'>
+        <div className="logo">
+            <span className='font-bold text-xl mx-8'>iTask</span>
+        </div>
+      <ul className="flex gap-8 mx-9">
+        <li className='cursor-pointer hover:font-bold transition-all duration-50'>Home</li>
+        <li className='cursor-pointer hover:font-bold transition-all duration-50'> Your Task</li>
+      </ul>
+    </nav>
+  )
+}
+
+
+
+
+export default Navbar
+
+
+
